3fa7f99f69ed2429f429f33bdb5e9e77db6e36b0
mimi77mimi <humboldt75@web.de>
2026-01-24 13:44:56 +0100

    einfacher Gegener ohne Funktionen

---
+++ scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		print("player dedectet")
+	else:
+		print("??? dedctet")
+	print("enter")
+	global_position += Vector3 (0,2,0)
